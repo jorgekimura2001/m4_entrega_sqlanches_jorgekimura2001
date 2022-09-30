0a386db8382841e526463701cef887408ac47dc6
Jorge Kimura <kimurajorge2001@gmail.com>
2022-09-30 18:46:06 -0300

    feat: entrega finalizada

--- crud.sql
+++ crud.sql
@@ -4,31 +4,57 @@
 -- Criação
 
 -- 1)
-
-
+INSERT INTO 
+	clientes(nome, lealdade) 
+VALUES
+	('Georgia', 0);
 
 -- 2)
-
-
+INSERT INTO
+	pedidos(status, cliente_id)
+VALUES 
+	('Recebido', (SELECT id FROM clientes WHERE nome ILIKE 'Geor%'));
 
 -- 3)
+INSERT INTO 
+	produtos_pedidos (pedido_id, produto_id)
+VALUES 
+	((SELECT id FROM clientes WHERE nome ILIKE 'Geor%'),(SELECT produtos.id FROM produtos WHERE produtos.nome ILIKE 'big%')),
+	((SELECT id FROM clientes WHERE nome ILIKE 'Geor%'),(SELECT produtos.id FROM produtos WHERE produtos.nome ILIKE 'VAR%')),
+	((SELECT id FROM clientes WHERE nome ILIKE 'Geor%'),(SELECT produtos.id FROM produtos WHERE produtos.nome ILIKE 'FRIT%')),
+	((SELECT id FROM clientes WHERE nome ILIKE 'Geor%'),(SELECT produtos.id FROM produtos WHERE produtos.nome ILIKE 'COC%')),
+	((SELECT id FROM clientes WHERE nome ILIKE 'Geor%'),(SELECT produtos.id FROM produtos WHERE produtos.nome ILIKE 'COCa%')); 
 
-
-
+SELECT * FROM produtos_pedidos WHERE pedido_id = (SELECT id FROM clientes WHERE nome ILIKE 'Geor%');
+	
 -- Leitura
 
 -- 1)
-
-
+SELECT c.id id_cliente, c.nome, c.lealdade, 
+p.id id_pedido, p.status, p.cliente_id, 
+po.id id_produto, po.nome, po.tipo, po.preço, po.pts_de_lealdade 
+FROM clientes c 
+JOIN pedidos p ON c.id = p.cliente_id 
+JOIN produtos_pedidos pp ON pp.pedido_id  = p.cliente_id 
+JOIN produtos po ON po.id = pp.produto_id 
+WHERE c.nome = 'Georgia';
 
 -- Atualização
 
 -- 1)
-
-
+UPDATE 
+	clientes 
+SET lealdade = (
+	SELECT SUM(po.pts_de_lealdade) 
+	FROM clientes c 
+	JOIN pedidos p ON c.id = p.cliente_id 
+	JOIN produtos_pedidos pp ON pp.pedido_id  = p.cliente_id 
+	JOIN produtos po ON po.id = pp.produto_id 
+	WHERE c.nome = 'Georgia'
+)
+WHERE nome = 'Georgia';
 
 -- Deleção
 
 -- 1)
-
-
+DELETE FROM clientes WHERE nome = 'Marcelo';
--- selects.sql
+++ selects.sql
@@ -2,11 +2,34 @@
 -- Seleções de dados
 
 -- 1)
+SELECT p.id, p.status, p.cliente_id, po.id, po.nome, po.tipo, po.preço, po.pts_de_lealdade  
+FROM produtos_pedidos pp
+JOIN pedidos p ON p.id = pp.pedido_id 
+JOIN produtos po ON po.id = pp.produto_id;
 
 -- 2)
+SELECT p.id FROM produtos_pedidos pp 
+JOIN pedidos p ON p.id = pp.pedido_id 
+JOIN produtos po ON po.id = pp.produto_id 
+WHERE po.nome = 'Fritas';
 
 -- 3)
+SELECT c.nome gostam_de_fritas FROM produtos_pedidos pp 
+JOIN pedidos p ON p.id = pp.pedido_id 
+JOIN produtos po ON po.id = pp.produto_id 
+JOIN clientes c ON p.cliente_id = c.id 
+WHERE po.nome = 'Fritas';
 
 -- 4)
+SELECT SUM(po.preço) FROM produtos_pedidos pp 
+JOIN pedidos p ON p.id = pp.pedido_id 
+JOIN produtos po ON po.id = pp.produto_id 
+JOIN clientes c ON p.cliente_id = c.id 
+WHERE c.nome = 'Laura';
 
 -- 5)
+SELECT po.nome, COUNT(po.nome) FROM produtos_pedidos pp 
+JOIN pedidos p ON p.id = pp.pedido_id 
+JOIN produtos po ON po.id = pp.produto_id 
+JOIN clientes c ON p.cliente_id = c.id 
+GROUP BY po.nome;
